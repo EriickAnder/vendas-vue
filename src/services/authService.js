27fd9931935c3235ca2937d1ca9f50c6521bd27d
Erick Anderson <eriickanderson@gmail.com>
2025-05-02 10:11:35 -0300

    Projeto Vendas

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import api from '../api/api';
 import { useAuthStore } from '../store/auth';
 
 export async function login() {
-    const email = 'alia.predovic@example.net';
+    const email = 'eriickanderson@gmail.com';
     const password = '123456';
     const response = await api.post('/login', { email, password });
     const authStore = useAuthStore();
